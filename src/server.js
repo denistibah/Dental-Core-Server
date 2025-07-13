d4fb4cda5608ae6b4cb551817f7054ab81fe8628
Denis Tibah <denistibah5@gmail.com>
2025-07-13 17:25:55 -0500

    remove port to deploy

--- src/server.js
+++ src/server.js
@@ -20,4 +20,4 @@ app.get('/', (req, res) => res.send('Denta API Running'));
 
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen();
